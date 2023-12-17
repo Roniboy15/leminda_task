2e28075c7c7628e0f20296035823ac7e76007869
Your Name <you@example.com>
2023-12-17 19:58:35 +0200

    beautified home

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,17 +1,18 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import './home.css'; 
 
 const Home = () => {
-
     const navigate = useNavigate();
     const [user] = useState(JSON.parse(localStorage.getItem('user')))
     const start = () => {
         navigate('/questions')
     }
+
     return (
-        <Container className="d-flex justify-content-center mt-3">
-            <Card className="text-center" style={{ width: '18rem' }}>
+        <Container className="justify-content-center align-items-center home-container">
+            <Card className="home-card text-center">
                 <Card.Body>
                     <Card.Title>Welcome to Our Questionnaire {user && user.username}!</Card.Title>
                     <Card.Text>
