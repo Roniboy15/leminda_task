9ccfb92603b4d02018e4819b31d0d57b4e7abd23
Your Name <you@example.com>
2023-12-17 19:59:46 +0200

    ok

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,19 +1,20 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import './home.css'; 
 
 const Home = () => {
-
     const navigate = useNavigate();
     const [user] = useState(JSON.parse(localStorage.getItem('user')))
     const start = () => {
         navigate('/questions')
     }
+
     return (
-        <Container className="d-flex justify-content-center mt-3">
-            <Card className="text-center" style={{ width: '18rem' }}>
+        <Container className="justify-content-center align-items-center home-container">
+            <Card className="home-card text-center">
                 <Card.Body>
-                    <Card.Title>Welcome to Our Questionnaire {user ? user.username : ''}!</Card.Title>
+                    <Card.Title>Welcome to Our Questionnaire {user && user.username}!</Card.Title>
                     <Card.Text>
                         We're so glad you're here. This questionnaire is designed to understand your preferences and thoughts.
                         Your input is valuable to us, and we appreciate the time you're taking to help us. Let's get started!
